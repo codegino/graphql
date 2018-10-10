a1af3f1ce4d86517d003570d5de9d51524413bc2
Gihooh <carloginocatapang@gmail.com>
2018-10-10 17:10:55 +0800

    Added subscription for post

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -64,7 +64,7 @@ const Mutation = {
 
     return user
   },
-  createPost(parent, args, {db}, info) {
+  createPost(parent, args, {db, pubsub}, info) {
     const userExists = db.users.some(user => user.id === args.data.author)
 
     if (!userExists) throw new Error('User does not exists.')
@@ -75,6 +75,10 @@ const Mutation = {
     }
 
     db.posts.push(post);
+
+    if(args.data.published) {
+      pubsub.publish(`post`, {post})
+    }
 
     return post
   },
